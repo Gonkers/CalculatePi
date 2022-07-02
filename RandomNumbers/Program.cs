966cca4011bf97408cf11fddea2af49fab80f499
Gonkers <github@gonkers.net>
2022-07-02 19:33:25 -0400

    Update to net6.0 & use Random.Shared

--- RandomNumbers/Program.cs
+++ RandomNumbers/Program.cs
@@ -3,54 +3,46 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace RandomNumbers
+namespace RandomNumbers;
+
+static class Program
 {
-    static class Program
+    static long Coprimes = 0;
+
+    static void Main(string[] args)
     {
-        // Start each run with a unique seed
-        static int seed = Environment.TickCount;
+        long count = -1;
 
-        // Ensure each thread has it's own Random with a unique seed
-        static readonly ThreadLocal<Random> Rng =
-            new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref seed)));
-
-        static int Coprimes = 0;
-
-        static void Main(string[] args)
+        Console.WriteLine("Calculating PI with random numbers!");
+        if (args.Length == 0 || !long.TryParse(args[0], out count))
         {
-            long count = -1;
-
-            Console.WriteLine("Calculating PI with random numbers!");
-            if (args.Length == 0 || !long.TryParse(args[0], out count))
-            {
-                Console.Write("How many random numbers should be used? ");
-                while (!long.TryParse(Console.ReadLine(), out count) && count <= 0) { }
-            }
-            var timer = Stopwatch.StartNew();
-
-            Parallel.For(0, count, (i) => GenerateCoprime());
-            timer.Stop();
-
-            Console.WriteLine();
-            Console.WriteLine($"Count: {count}\tCofactors: {count - Coprimes}\tCoprimes: {Coprimes}\tTotal Time: {timer.ElapsedMilliseconds}ms");
-            var pi = Math.Sqrt(6 / (Coprimes / (double)count));
-            Console.WriteLine($"PI ~= {pi}");
+            Console.Write("How many random numbers should be used? ");
+            while (!long.TryParse(Console.ReadLine(), out count) && count <= 0) { }
         }
+        var timer = Stopwatch.StartNew();
 
-        static bool TryGenerateCoprime() => gcd(Rng.Value.Next(), Rng.Value.Next()) == 1;
+        Parallel.For(0, count, (_) => GenerateCoprime());
+        timer.Stop();
 
-        static void GenerateCoprime()
-        {
-            if (TryGenerateCoprime())
-                Interlocked.Increment(ref Coprimes);
-        }
+        Console.WriteLine();
+        Console.WriteLine($"Count: {count}\tCofactors: {count - Coprimes}\tCoprimes: {Coprimes}\tTotal Time: {timer.ElapsedMilliseconds}ms");
+        var pi = Math.Sqrt(6 / (Coprimes / (double)count));
+        Console.WriteLine($"PI ~= {pi}");
+    }
 
-        static int gcd(int x, int y)
-        {
-            if (y == 0)
-                return x;
-            else
-                return gcd(y, x % y);
-        }
+    static bool TryGenerateCoprime() => gcd(Random.Shared.Next(), Random.Shared.Next()) == 1;
+
+    static void GenerateCoprime()
+    {
+        if (TryGenerateCoprime())
+            Interlocked.Increment(ref Coprimes);
+    }
+
+    static long gcd(long x, long y)
+    {
+        if (y == 0)
+            return x;
+        else
+            return gcd(y, x % y);
     }
 }
